a82b6c0adc33f862ae7cf7efe12c67f27148714b
Andreea Dima <andreea.dima@1and1.ro>
2018-09-07 14:15:40 +0300

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'domain-connect-dyndns',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Python client library for Dynamic DNS using Domain Connect',
     license = 'MIT',
     long_description=long_description,
